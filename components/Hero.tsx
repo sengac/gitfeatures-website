17c06f8ddc1ee0aa5fa087356a06dfb93c832915
Roland Quast <rquast@rolandquast.com>
2021-03-29 16:41:59 +1000

    bump version

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -47,7 +47,7 @@ const Hero = () => {
                         color="gray.500"
                         textTransform="uppercase"
                     >
-                        Pre-alpha Release v0.0.1-alpha.10
+                        Pre-alpha Release v0.0.1-alpha.11
                     </chakra.p>
                     <Center>
                         <Stack p={6} isInline wrap="wrap">
